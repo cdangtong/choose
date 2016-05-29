a431eee4847e41ff17c2acea78de55dbd5674755
陈英琪 <chenyingqi@chenyingqidemacbook-air.local>
2016-05-29 23:52:12 +0800

    Initial Commit

---
+++ choose/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  choose
+//
+//  Created by 陈英琪 on 16/5/29.
+//  Copyright © 2016年 陈英琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ choose/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  choose
+//
+//  Created by 陈英琪 on 16/5/29.
+//  Copyright © 2016年 陈英琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
